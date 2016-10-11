ab2410a2ea7551f6f00faf1bfd9c52d111bfc75d
Bessey, Michael <MBessey@MassMutual.com>
2016-10-11 18:44:27 -0400

    utility function

--- mmviz_utils.py
+++ mmviz_utils.py
@@ -1,3 +1,5 @@
+import matplotlib
+
 # create bin list based on values list and bin_width
 def create_bin_list(values, bin_width = 30):
 
@@ -10,3 +12,14 @@ def create_bin_list(values, bin_width = 30):
     bin_list = [i for i in range(min_bin, max_bin, bin_width)]
     return bin_list
 
+# create space in the chart for the legend
+def place_legend(plt, title, width = 0.2) :
+
+    plt.legend(title = title, bbox_to_anchor=(1, 0.5), loc='center left', ncol=1)
+    plt.subplots_adjust(right= 1 - width)
+
+
+def theme_mm(chart_type = "default") :
+
+    if chart_type == "bar" :
+        matplotlib.rc('lines', linewidth=2, color='r')
