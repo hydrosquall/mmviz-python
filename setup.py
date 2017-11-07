fb73140bbbf650fff94e52d8c9347693d04c69e2
Zheng, Yuhang <YZheng83@MassMutual.com>
2017-11-07 11:09:31 -0500

    create setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='mmviz',
+    version='1.0.0',
+    packages=['mmviz-python'],
+    url='https://github.com/massmutual/mmviz-python',
+    license='MIT',
+    author='Michael Bessey',
+    author_email='mbessey@massmutual.com',
+    description='MassMutual Visualization Python Package'
+)
